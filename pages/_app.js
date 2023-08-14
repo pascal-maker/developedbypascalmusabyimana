ecdbc7afcec3671820bf661cce655127a5368dcd
pascal-maker <pascalmusabyimana681@gmail.com>
2023-08-15 00:07:54 +0200

    yes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import React, { useEffect } from 'react';
+import '@alan-ai/alan-sdk-web'; // Import the Alan SDK
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      alanBtn({
+        key: '474d9c872d0411af8af62758b96035122e956eca572e1d8b807a3e2338fdd0dc/stage', // Replace with your API key
+        onCommand: (commandData) => {
+          if (commandData.command === 'go:back') {
+            // Call the client code that will react to the received command
+          }
+        }
+      });
+    }
+  }, []); // Empty dependency array to ensure it runs once
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
