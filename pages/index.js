c8ceea51bcb5584541373a8361a6a9c4fdf928e8
pascal-maker <pascalmusabyimana681@gmail.com>
2023-11-07 23:06:04 +0100

    yes

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
             I have 3 years of experience building and designing software.
-             Currently, I love to work on web and  mobile applications using technologies like React, Laravel, React-native , Objective-C, Java ,Javascript Firebase and Swift.
+             Currently, I love to work on web and  mobile applications or computer-vision related projects using technologies like React, Laravel, React-native , OPENCV, Ultralytics, Yolov8, Objective-C, Java ,Javascript Firebase and Swift.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">     
     <a href="https://twitter.com/pascalmusabyim1?lang=en">< FaXTwitter/></a>
