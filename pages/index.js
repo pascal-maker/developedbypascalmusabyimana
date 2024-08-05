771477d9b4dc3e8f22b53acf3ec54c3218adbdf0
pascal-maker <pascal-musa@hotmail.com>
2024-08-05 12:42:24 +0200

    new updates added chatbotkit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/pascalwhitepants.jpg";
+import deved from "../public/pascalamsterdam.jpg";
 import code from "../public/code.png";
 import design from "../public/fullstack.jpg";
 import consulting from "../public/consulting.png";
