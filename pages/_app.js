0ad4be740b6fcdea75690df2505a80fd131bb5b1
pascal-maker <pascal-musa@hotmail.com>
2024-08-05 12:36:22 +0200

    new updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+// pages/_app.js
 import React from 'react';
 import '../styles/globals.css';
+import '../styles/CustomCopilot.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
