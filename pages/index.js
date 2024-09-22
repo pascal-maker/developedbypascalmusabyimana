c7c3e5e4440e9e6eca4cd2bb50d584746dfc5837
pascal-maker <pascal-musa@hotmail.com>
2024-09-22 21:50:48 +0200

    no resume just linkedinfile

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/api/getResume"
+                  href="https://www.linkedin.com/in/pascal-musabyimana-573b66178/overlay/1727034581247/single-media-viewer/?profileId=ACoAACoslacB56MwdRN6bL1P1uM5qLdxwYzUcg8"
                 >
                   Resume
                 </a>
