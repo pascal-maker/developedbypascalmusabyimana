96dfe2c953390565b4d617e2a7cd2b348000abed
pascal-maker <pascal-musa@hotmail.com>
2024-09-22 21:32:26 +0200

    pascal pdf updated

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/pascal.pdf"
+                  href="C:\Users\pasca\developedbypascalmusabyimana\pascal.pdf"
                 >
                   Resume
                 </a>
