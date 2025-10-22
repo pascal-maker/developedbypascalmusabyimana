ed3f13414ceadcd7891826062cfe2becd6437900
pascal-maker <pascal-musa@hotmail.com>
2025-10-22 22:57:03 +0200

    Update resume button to link to CV PDF

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,9 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://www.linkedin.com/in/pascal-musabyimana-573b66178/o"
+                  href="/Pascal_Musabyimana_ModernTech_CV.Pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Resume
                 </a>
