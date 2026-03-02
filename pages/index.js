adb506ecf576e7859f27e308b88bb2b10235775c
pascal-maker <pascal-musa@hotmail.com>
2026-03-03 00:39:23 +0100

    fix: correct case on Valencia.JPG import

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/Valencia.JPG";
+import deved from "../public/valencia.JPG";
 import code from "../public/code.png";
 import design from "../public/fullstack.jpg";
 import consulting from "../public/consulting.png";
