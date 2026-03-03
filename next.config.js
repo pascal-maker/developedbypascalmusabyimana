353618d60e284671dfacf49994595ec239728375
pascal-maker <pascal-musa@hotmail.com>
2026-03-03 01:46:33 +0100

    feat: add GitHub Actions deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove the 'alan-client' static path block
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
